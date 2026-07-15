e22b58afad168f9515a7c72175b22135891f68ad
Nacips <shipa.arr07@gmail.com>
2026-07-15 13:45:48 +0700

    Delete Bioskop directory

--- Bioskop/app/src/main/java/com/example/stikomcinema/model/FilmItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.stikomcinema.model
-
-data class FilmItem(
-    val id: Int,
-    val title: String,
-    val studio: String,
-    val schedule: String,
-    val price: Int
-)
--- Bioskop/app/src/main/java/com/example/stikomcinema/model/FoodItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.stikomcinema.model
-
-data class FoodItem(
-    val id: Int,
-    val name: String,
-    val category: String,
-    val price: Int
-)
